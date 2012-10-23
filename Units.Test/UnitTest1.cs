4037bf755c0ea573644073feb570491a1ed622a9
Ivan Perez <irperez@gmail.com>
2012-10-22 22:52:02 -0400

    Added code to load up the data file & unit types

---
+++ Units.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Units;
+
+namespace Units.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            UnitProvider up = new UnitProvider();
+        }
+    }
+}
--- Units/UnitProvider.cs
+++ Units/UnitProvider.cs
@@ -1,17 +1,32 @@
 ﻿using System;
-using System.Data.SqlClient;
-using System.Configuration;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Xml;
 
 namespace Units
 {
-    public class UnitProvider 
+    public class UnitProvider
     {
 
         #region "Constructors"
         public UnitProvider()
         {
+            if (_dataFile == null)
+            {
+                this.LoadDataFile();
+            }
+        }
+        #endregion
+
+        #region "Properties"
+        static private XmlDocument _dataFile;
+        protected XmlDocument DataFile 
+        { 
+            get{return _dataFile;}
+            set{ _dataFile = value;}
         }
         #endregion
 
@@ -25,28 +40,97 @@ namespace Units
             {
                 if (_unitTypes == null)
                 {
-                    _unitTypes = GetAllUnitTypes();
+                    GetAllUnitTypes();
                 }
                 return _unitTypes;
             }
         }
 
+        private void LoadDataFile()
+        {
+            string fileName = "Units.UnitData.xml";
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            var stream = assembly.GetManifestResourceStream(fileName);
+
+            if (stream == null)
+            {
+                throw new FileNotFoundException("Cannot find unit data file", fileName);
+            }
+
+            _dataFile = new XmlDocument();
+            _dataFile.Load(stream);
+
+            this.GetAllUnitTypes();
+        }
+
         //TODO: Change to ReadOnlyDictionary when .Net 4.0 is available
-        private Dictionary<string, UnitType> GetAllUnitTypes()
-		{
-			Dictionary<string, UnitType> unitTypes = new Dictionary<string, UnitType>();
+        private void GetAllUnitTypes()
+        {
+            _unitTypes = new Dictionary<string, UnitType>();
 
-            //using (UnitEntities context = new UnitEntities(GenerateEntityConnection(DefaultConnectionName))) {
-                //object query = from ut in context.UnitTypes.Include("Units.Modifiers").Include("Units.Sources").Include("Units.Symbols") orderby ut.Name select ut;
+            ProcessUnitConverterData();
+        }
 
-                //foreach (var itm in query) {
-                //    unitTypes.Add(itm.Name, itm);
-                //}
+        private void ProcessUnitConverterData()
+        {
+            foreach (XmlNode node in this.DataFile.ChildNodes)
+            {
+                switch (node.Name)
+                {
+                    case "UnitConverterData":
+                        ProcessDataTypes(node);
+                        break;
+                    case "test 2":
+                        int j = 0;
+                        j++;
+                        break;
+                }
+            }
+        }
 
-            //}
+        private void ProcessDataTypes(XmlNode parentNode)
+        {
+            foreach (XmlNode node in parentNode.ChildNodes)
+            {
+                switch (node.Name)
+                {
+                    case "UnitTypes":
+                        ProcessUnitTypes(node);
+                        break;
+                }
+            }
+        } 
 
-			return unitTypes;
-		}
+        private void ProcessUnitTypes(XmlNode parentNode)
+        {
+            foreach (XmlNode node in parentNode.ChildNodes)
+            {
+                if(node.Name == "UnitType")
+                {
+                    UnitType ut = new UnitType();
+                    
+                    foreach (XmlAttribute attrib in node.Attributes)
+                    {
+                        switch (attrib.Name)
+                        {
+                            case "ID":
+                                ut.ID = Convert.ToInt32(attrib.Value);
+                                break;
+                            case "Name":
+                                ut.Name = attrib.Value;
+                                break;
+                            case "Description":
+                                ut.Description = attrib.Value;
+                                break;
+                        }
+                        
+                    }
+
+                    _unitTypes.Add(ut.Name, ut);
+
+                }
+            }
+        }
         #endregion
 
         #region "Units"
@@ -66,26 +150,29 @@ namespace Units
 
         //TODO: Change to ReadOnlyDictionary when .Net 4.0 is available
         private Dictionary<string, Unit> GetAllUnits()
-		{
-			Dictionary<string, Unit> unitDict = new Dictionary<string, Unit>();
-			UnitProvider unitPro = new UnitProvider();
-			var query = from s in unitPro.UnitTypes select s;
+        {
+            Dictionary<string, Unit> unitDict = new Dictionary<string, Unit>();
+            UnitProvider unitPro = new UnitProvider();
+            var query = from s in unitPro.UnitTypes select s;
 
 
-			foreach (var itm in query) {
-				UnitType ut = itm.Value;
+            foreach (var itm in query)
+            {
+                UnitType ut = itm.Value;
 
-				foreach (var un in ut.Units) {
-					if (!unitDict.ContainsKey(un.Name)) {
-						unitDict.Add(un.Name, un);
-					}
-				}
-				//Units "Celsius"
-			}
-			//UnitTypes "Temperature"
+                foreach (var un in ut.Units)
+                {
+                    if (!unitDict.ContainsKey(un.Name))
+                    {
+                        unitDict.Add(un.Name, un);
+                    }
+                }
+                //Units "Celsius"
+            }
+            //UnitTypes "Temperature"
 
-			return unitDict;
-		}
+            return unitDict;
+        }
         #endregion
 
         #region "Symbols"
@@ -118,75 +205,88 @@ namespace Units
 
         //TODO: Change to ReadOnlyDictionary when .Net 4.0 is available
         private Dictionary<string, Unit> GetAllSymbols()
-		{
-			Dictionary<string, Unit> unitDict = new Dictionary<string, Unit>();
-			UnitProvider unitPro = new UnitProvider();
+        {
+            Dictionary<string, Unit> unitDict = new Dictionary<string, Unit>();
+            UnitProvider unitPro = new UnitProvider();
 
-			var query = from s in unitPro.UnitTypes select s;
-
-
-			foreach (var itm in query) {
-				UnitType ut = itm.Value;
+            var query = from s in unitPro.UnitTypes select s;
 
 
-				foreach (var un in ut.Units) {
-					Unit unit = un;
+            foreach (var itm in query)
+            {
+                UnitType ut = itm.Value;
 
-					if (unit.Symbols == null || unit.Symbols.Count == 0) {
-						//No symbols.  Add name of unit as symbol.
-						if (!unitDict.ContainsKey(unit.Name)) {
-							unitDict.Add(unit.Name, unit);
-						}
-					} else {
-						foreach (var sy in unit.Symbols) {
-							if (!unitDict.ContainsKey(sy.Value)) {
-								unitDict.Add(sy.Value, unit);
-							}
-						}
-						//Symbols "C"
 
-						if (!unitDict.ContainsKey(unit.Name)) {
-							unitDict.Add(unit.Name, unit);
-							//Add name as symbol just in case
-						}
-					}
-				}
-				//Units "Celsius"
-			}
-			//UnitTypes "Temperature"
+                foreach (var un in ut.Units)
+                {
+                    Unit unit = un;
 
-			return unitDict;
-		}
+                    if (unit.Symbols == null || unit.Symbols.Count == 0)
+                    {
+                        //No symbols.  Add name of unit as symbol.
+                        if (!unitDict.ContainsKey(unit.Name))
+                        {
+                            unitDict.Add(unit.Name, unit);
+                        }
+                    }
+                    else
+                    {
+                        foreach (var sy in unit.Symbols)
+                        {
+                            if (!unitDict.ContainsKey(sy.Value))
+                            {
+                                unitDict.Add(sy.Value, unit);
+                            }
+                        }
+                        //Symbols "C"
+
+                        if (!unitDict.ContainsKey(unit.Name))
+                        {
+                            unitDict.Add(unit.Name, unit);
+                            //Add name as symbol just in case
+                        }
+                    }
+                }
+                //Units "Celsius"
+            }
+            //UnitTypes "Temperature"
+
+            return unitDict;
+        }
 
 
         //TODO: Change to ReadOnlyDictionary when .Net 4.0 is available
         private Dictionary<string, Symbol> GetAllIndividualSymbols()
-		{
-			Dictionary<string, Symbol> unitDict = new Dictionary<string, Symbol>();
-			UnitProvider unitPro = new UnitProvider();
+        {
+            Dictionary<string, Symbol> unitDict = new Dictionary<string, Symbol>();
+            UnitProvider unitPro = new UnitProvider();
 
-			var query = from s in unitPro.UnitTypes select s;
-
-
-			foreach (var itm in query) {
-				UnitType ut = itm.Value;
+            var query = from s in unitPro.UnitTypes select s;
 
 
-				foreach (var un in ut.Units) {
-					Unit unit = un;
+            foreach (var itm in query)
+            {
+                UnitType ut = itm.Value;
 
-					foreach (var us in unit.Symbols) {
-						if (!unitDict.ContainsKey(us.Value)) {
-							unitDict.Add(us.Value, us);
-						}
-					}
 
-				}
+                foreach (var un in ut.Units)
+                {
+                    Unit unit = un;
 
-			}
+                    foreach (var us in unit.Symbols)
+                    {
+                        if (!unitDict.ContainsKey(us.Value))
+                        {
+                            unitDict.Add(us.Value, us);
+                        }
+                    }
 
-			return unitDict;
-		}
+                }
+
+            }
+
+            return unitDict;
+        }
 
         #endregion
 
