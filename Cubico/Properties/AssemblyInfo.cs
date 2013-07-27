83386e239177321f8d994deda39fc2d7ca117704
Taylor Shuler <taylorshuler@aol.com>
2013-07-27 11:36:37 -0400

    Updated assemblies

--- Cubico/Properties/AssemblyInfo.cs
+++ Cubico/Properties/AssemblyInfo.cs
@@ -1,37 +1,20 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Cubico")]
-[assembly: AssemblyDescription("A measurement and unit conversion library.")]
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Cubico.Data")]
+[assembly: AssemblyDescription("An extension to Cubico for virtual unit conversions.")]
 //[assembly: AssemblyConfiguration("")]
 //[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cubico")]
-//[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("Cubico.Data")]
+[assembly: AssemblyCopyright("Ivan R. Perez, Taylor Shuler")]
 //[assembly: AssemblyTrademark("")]
 //[assembly: AssemblyCulture("")]
-//[assembly: InternalsVisibleTo("Cubico.Tests")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("23dde50a-9f57-40aa-8c44-b41368072214")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("0.1.*")]
-//[assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyVersion("1.0.*")]
